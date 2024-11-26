48f43bb17bc1d483b1a0818aa5c6d0c2e6a10897
shahinexy <shahinexy@gmail.com>
2024-11-26 22:32:27 +0600

    hook form added

--- src/Pages/Authentication/Login.jsx
+++ src/Pages/Authentication/Login.jsx
@@ -1,5 +1,15 @@
 
+import { useForm } from "react-hook-form";
+
 const Login = () => {
+
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
+
+
+
     return (
 <div>
             <div className="hero bg-base-200 min-h-screen">
@@ -12,18 +22,22 @@ const Login = () => {
       </p>
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
+      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input
+          {...register("email")}
+          type="email" placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input
+          {...register("password")}
+          type="password" placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
