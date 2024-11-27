5cdb53b4409b65f6307c577421099bd7eaebd917
shahinexy <shahinexy@gmail.com>
2024-11-27 22:07:41 +0600

    env file created

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2nacjFAiAfq7yuY4wnqH0fMlH9yioutE",
-  authDomain: "shining-car-8e82c.firebaseapp.com",
-  projectId: "shining-car-8e82c",
-  storageBucket: "shining-car-8e82c.firebasestorage.app",
-  messagingSenderId: "367309435874",
-  appId: "1:367309435874:web:34ec035bfa2749c31c3fbb"
+  authDomain: import.meta.env.VITE_authDomain,
+  apiKey: import.meta.env.VITE_apiKey,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
