77dc17ab7b4feb12a00f6427978a7a5251eb6845
shahinexy <shahinexy@gmail.com>
2024-12-02 11:50:36 +0600

    get single user data success

--- src/Pages/UserDatas/UserData.jsx
+++ src/Pages/UserDatas/UserData.jsx
@@ -5,7 +5,11 @@ const UserData = () => {
     console.log(data);
     return (
         <div>
-            this is user data pages {data?.userEmail}
+            <h1 className="text-center text-3xl text-green-700 font-bold">Users Data</h1>
+            <div className="text-center">
+                <p>User Email: {data?.userEmail}</p>
+                <p>User Password: {data?.userPass}</p> 
+            </div>
         </div>
     );
 };
