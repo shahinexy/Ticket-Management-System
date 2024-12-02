10023e3e947c39a7c6efab040fd2037084e21ecf
shahinexy <shahinexy@gmail.com>
2024-12-02 10:13:37 +0600

    get data from databes success

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,27 @@
+import {useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Home = () => {
+    const {data, isPending} = useQuery({
+        queryKey: ['datas'],
+        queryFn: async () =>{
+           const res = await axios.get('http://localhost:3000/alluser')
+           return res.data
+        }
+    })
+
+    if(isPending) return <div className="dav">loading</div>
+
+    console.log(data);
     return (
         <div>
-            thsiis home
+            <h1 className="text-center text-3xl text-green-700 font-bold">Users Data</h1>
+
+            {
+                data.map((data) => <div key={data._id} className="">
+                    <h3>Email: {data.userEmail}</h3>
+                </div> )
+            }
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,18 @@ import {
 } from "react-router-dom";
 import router from './Router/router';
 import AuthProvider from './Provider/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
-
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
+    </QueryClientProvider>
+
   </StrictMode>,
 )
