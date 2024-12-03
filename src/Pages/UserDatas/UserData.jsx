1613593acab9610cc42a372c75961666f0a11b81
shahinexy <shahinexy@gmail.com>
2024-12-03 23:57:54 +0600

    some modifications

--- src/Pages/UserDatas/UserData.jsx
+++ src/Pages/UserDatas/UserData.jsx
@@ -7,8 +7,8 @@ const UserData = () => {
         <div>
             <h1 className="text-center text-3xl text-green-700 font-bold">Users Data</h1>
             <div className="text-center">
-                <p>User Email: {data?.userEmail}</p>
-                <p>User Password: {data?.userPass}</p> 
+                <p className="bg-green-600 p-5 text-white mb-5">User Email: {data?.userEmail}</p>
+                <p className="bg-green-600 p-5 text-white mb-5">User Password: {data?.userPass}</p> 
             </div>
         </div>
     );
